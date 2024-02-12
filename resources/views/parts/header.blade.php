eb64d937ba7f9102b01551df5af591321b338bca
azumiyu <azumiyukihiro517@icloud.com>
2024-02-12 09:30:52 +0900

    viewテスト

---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,16 @@
+<div class="fixed-top header text-center">
+    <h1>草野球予定</h1>
+</div>
+<div id="header" class="fixed-top sub_header">
+    <div>
+        <button type="button" class="btn btn-primary mt-2">予定新規作成</button>
+    </div>
+    <div class="mt-2">
+        <div class="input-group">
+            <input type="text" id="txt-search" class="form-control input-group-prepend" placeholder="キーワードを入力"></input>
+            <span class="input-group-btn input-group-append">
+                <submit type="submit" id="btn-search" class="btn btn-primary"><i class="fas fa-search"></i> 検索</submit>
+            </span>
+        </div>
+    </div>
+</div>
