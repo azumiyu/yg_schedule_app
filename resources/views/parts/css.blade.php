314b4fbd83213550aac3c01c84d019dbef19f020
azumiyu <azumiyukihiro517@icloud.com>
2024-02-12 10:26:01 +0900

    testView2

---
+++ resources/views/parts/css.blade.php
@@ -0,0 +1,49 @@
+<style>
+/* css */
+
+.header {
+    height:100px; background-color: #f5af7d;
+    padding-top: 20px;
+}
+
+.sub_header{
+    height:100px;
+    background-color:#d4ebec;
+    margin-top:100px;
+    display: flex;
+    justify-content:space-around;
+}
+
+.search {
+    width:150px;
+}
+
+.container {
+    margin-top:200px;
+}
+
+.game_header p{
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+
+a {
+    text-decoration: none;
+    color: inherit;
+}
+
+table {
+    font-size: x-small;
+}
+.kaisai {
+    background-color: aquamarine;
+}
+.syuryou {
+    background-color: #cccccc;
+}
+.uketuketyu {
+    background-color: white;
+}
+
+</style>
---
+++ resources/views/parts/table.blade.php
@@ -0,0 +1,31 @@
+<div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">タイトルu</th>
+                    <th scope="col">場所</th>
+                    <th scope="col">日時</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="table-success">
+                    <th scope="row">1</th>
+                    <td>練習試合ああああああ</td>
+                    <td>やまだ第二うどんぐっちゃぐちゃ卵ふりかけ球場</td>
+                    <td>2/21(土)17:00~</td>
+                </tr>
+                <tr class="table-secondary">
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+        </tr>
+</tbody>
+</table>
+</div>
